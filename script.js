fb4291bfe7d00070f0596d80f1a46d943ae0fcdd
CaioIack <caio.iack1@gmail.com>
2022-08-11 16:02:25 -0300

    Criacao de funcao de login

---
+++ script.js
@@ -0,0 +1,12 @@
+function loginTryber() {
+    let email = document.getElementsByName('email')[0].value;
+    let password = document.getElementsByName('password')[0].value;
+    
+    if(email === 'tryber@teste.com' && password === '123456') {
+        alert('Olá, Tryber');
+    } else {
+        alert('Email ou senha inválidos');
+    }
+}
+
+document.getElementById('login').addEventListener('click', loginTryber);
