d68ad499ed76e19d7ba5a2ca5262255c361f9881
Matheus Chaves de Farias <matheusc.farias123@gmail.com>
2022-08-11 23:08:32 -0300

    Ajustando ESLint do script

--- script.js
+++ script.js
@@ -1,40 +1,40 @@
 function loginTryber() {
-    let email = document.getElementsByName('email')[0].value;
-    let password = document.getElementsByName('password')[0].value;
-    
-    if(email === 'tryber@teste.com' && password === '123456') {
-        alert('Olá, Tryber!');
-    } else {
-        alert('Email ou senha inválidos.');
-    }
+  const email = document.getElementsByName('email')[0].value;
+  const password = document.getElementsByName('password')[0].value;
+
+  if (email === 'tryber@teste.com' && password === '123456') {
+    alert('Olá, Tryber!');
+  } else {
+    alert('Email ou senha inválidos.');
+  }
 }
 
 document.getElementById('loginBTN').addEventListener('click', loginTryber);
 
-for(let index=1; index<11; index+=1) {
-    let div = document.createElement('div');
-    document.getElementById('nota').appendChild(div);
-    let input = document.createElement('input');
-    input.type = 'radio';
-    input.name = 'rate';
-    input.id = 'rate'+index;
-    input.value = index;
-    div.appendChild(input);
-    let label = document.createElement('label');
-    label.setAttribute('for', 'rate'+index);
-    label.innerText = index;
-    div.appendChild(label);
+  for(let index = 1; index < 11; index += 1) {
+  const div = document.createElement('div');
+  document.getElementById('nota').appendChild(div);
+  const input = document.createElement('input');
+  input.type = 'radio';
+  input.name = 'rate';
+  input.id = 'rate' + index;
+  input.value = index;
+  div.appendChild(input);
+  const label = document.createElement('label');
+  label.setAttribute('for', 'rate' + index);
+  label.innerText = index;
+  div.appendChild(label);
 }
 
 document.getElementById('agreement').addEventListener('change', function (event) {
-    if(event.target.checked === true) {
-        document.getElementById('submit-btn').disabled = false;
-    } else {
-        document.getElementById('submit-btn').disabled = true;
-    }
+  if (event.target.checked === true) {
+    document.getElementById('submit-btn').disabled = false;
+  } else {
+    document.getElementById('submit-btn').disabled = true;
+  }
 })
 
 document.getElementById('comentario').addEventListener('input', function (event) {
-    let counter = document.getElementById('counter');
-    counter.innerText = 500-Number(event.target.value.length)
+  const counter = document.getElementById('counter');
+  counter.innerText = 500-Number(event.target.value.length)
 })
